aa8fd4d37c32b7f2832e41776a45e365f5db0b80
Juan Torres <jmmtorres1991@yahoo.com>
2020-06-14 02:04:10 -0500

    Clean bare bone project
    Added view controllers to storyboard.

---
+++ Project38/Project38/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  Project38
+//
+//  Created by Juan Torres on 6/14/20.
+//  Copyright © 2020 Juan Torres. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var detailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
