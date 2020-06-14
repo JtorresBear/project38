73605a5720635568ddadc6c108f18d1eda5643be
Juan Torres <jmmtorres1991@yahoo.com>
2020-06-14 01:39:04 -0500

    first creation
    Just starter files.

---
+++ Project38/Project38/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project38
+//
+//  Created by Juan Torres on 6/14/20.
+//  Copyright © 2020 Juan Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
