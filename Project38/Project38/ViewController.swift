8c750c5b7b43fd1f0458ac8002e9dc9cc85b4e8d
Juan Torres <jmmtorres1991@yahoo.com>
2020-06-16 17:57:33 -0500

    added load saved data function
    builds fine. no errors.

--- Project38/Project38/ViewController.swift
+++ Project38/Project38/ViewController.swift
@@ -20,6 +20,8 @@ import CoreData
 class ViewController: UITableViewController {
     //see top^^^
     var container: NSPersistentContainer!
+    //objects that are to be saved to and loaded from
+    var commits = [Commit]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +36,8 @@ class ViewController: UITableViewController {
         }
         
         performSelector(inBackground: #selector(fetchCommits), with: nil)
+        
+        loadSavedData()
     }
     
     func saveContext(){
@@ -65,6 +69,7 @@ class ViewController: UITableViewController {
                 }
                 
                 self.saveContext()
+                self.loadSavedData()
             }
         }
     }
@@ -79,6 +84,43 @@ class ViewController: UITableViewController {
         let formatter = ISO8601DateFormatter()
         commit.date = formatter.date(from: json["commit"]["commiter"]["date"].stringValue) ?? Date()
         
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commits.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Commit", for: indexPath)
+        
+        let commit = commits[indexPath.row]
+        cell.textLabel!.text = commit.message
+        cell.detailTextLabel!.text = commit.date.description
+        
+        return cell
+    }
+    
+    func loadSavedData(){
+        //creates an NSRequest
+        let request = Commit.createFetchRequest()
+        //creates a sort of the date data in Commit object
+        //date is the key or Commit.date and it sorts it in descending order
+        let sort = NSSortDescriptor(key: "date", ascending: false)
+        //uses the sort we initialized
+        request.sortDescriptors = [sort]
+        
+        do{
+            //loads the commits into the commits array using the request we specified
+            commits = try container.viewContext.fetch(request)
+            print("Got\(commits.count) commits")
+            tableView.reloadData()
+        } catch {
+            print("Fetch Failed")
+        }
     }
 
 }
